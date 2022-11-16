3d26427d83435fd7fa224c62bdb58c03f654e5c5
eadortsu <eugeneadortsu@gmail.com>
2022-11-16 15:28:19 +0000

    auth setup

---
+++ src/admins/entities/admin.entity.ts
@@ -0,0 +1,53 @@
+import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+export class Admin extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  first_name: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  last_name: string;
+
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  phone: string;
+
+  @Column({ type: 'varchar', nullable: true, unique: true })
+  email: string;
+
+  @Column({ default: false, type: 'boolean' })
+  phone_verified: boolean;
+
+  @Column({ default: false, type: 'boolean' })
+  email_verified: boolean;
+
+  @Column({ type: 'varchar', nullable: true })
+  photo: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  username: string;
+
+  @Column({ nullable: true })
+  gender: string;
+
+  @Column({ nullable: true })
+  dob: Date;
+
+  @Column({ type: 'varchar', default: 'Agent' })
+  role: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  reset_code: string;
+
+  @Column()
+  salt: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
+}
